c5eb26bcf550fb00703a989c9f74f94b6c00c862
terencerl <terence.lobo@stfc.ac.uk>
2026-08-28 17:22:33 +0100

    adding the common header to the osem 3d particles code

---
+++ apps/c/oSEM_3d_particles/common.h
@@ -0,0 +1,13 @@
+#ifndef _OSEM3D_COMMON_H_
+#define _OSEM3D_COMMON_H_
+
+#define NCOMP 7
+#define E_X 0 
+#define E_Y 1
+#define E_Z 2
+#define E_R 3 
+#define E_SX 4
+#define E_SY 5
+#define E_SZ 6
+
+#endif 
